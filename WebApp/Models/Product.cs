6e6d1e65f19f1a5636dbd807c25f75ef76322964
Sebastian_Puruguay <20100770@ue.edu.pe>
2023-05-19 09:00:14 -0500

    Agregar archivos de proyecto.

---
+++ WebApp/Models/Product.cs
@@ -0,0 +1,20 @@
+﻿namespace WebApp.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string? Description { get; set; }
+
+        public string? ImageUrl { get; set; }
+
+        public int Stock { get; set; }
+
+        public decimal Price { get; set; }
+
+        public double Discount { get; set; } 
+        public string? IsActive { get; set; }
+
+
+    }
+}
---
+++ WebApp/Models/WebContext.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApp.Models
+{
+    public partial class WebContext : DbContext
+    {
+        public WebContext()
+        {
+
+        }
+
+        public WebContext(DbContextOptions<WebContext> options) : base(options)
+        {
+
+        }
+        public virtual DbSet<Product> Product { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseSqlServer("Server=LAPTOP-A8RA8SB1;Database=PruebaWeb;Integrated Security=true;TrustServerCertificate=True");
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(e => e.Description).HasMaxLength(300);
+                entity.Property(e => e.ImageUrl).HasMaxLength(300);
+                
+            });
+
+            base.OnModelCreating(modelBuilder);
+        }
+
+        partial void onModelCreatPartial(ModelBuilder modelBuilder);
+    }
+}
